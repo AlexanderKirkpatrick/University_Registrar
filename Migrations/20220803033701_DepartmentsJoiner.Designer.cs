917951fd05f5e5c2b37277d220b86cfac8fa7648
Alexander <knibz@protonmail.com>
2022-08-03 01:04:33 -0400

    Add updated DB info

--- Migrations/20220803033701_DepartmentsJoiner.Designer.cs
+++ Migrations/20220803033701_DepartmentsJoiner.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using University_Registrar.Models;
 
 namespace University_Registrar.Migrations
 {
     [DbContext(typeof(University_RegistrarContext))]
-    partial class University_RegistrarContextModelSnapshot : ModelSnapshot
+    [Migration("20220803033701_DepartmentsJoiner")]
+    partial class DepartmentsJoiner
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20220803033701_DepartmentsJoiner.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace University_Registrar.Migrations
+{
+    public partial class DepartmentsJoiner : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Departments",
+                columns: table => new
+                {
+                    DepartmentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departments", x => x.DepartmentId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DepartmentStudent",
+                columns: table => new
+                {
+                    DepartmentStudentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    StudentId = table.Column<int>(type: "int", nullable: false),
+                    DepartmentId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DepartmentStudent", x => x.DepartmentStudentId);
+                    table.ForeignKey(
+                        name: "FK_DepartmentStudent_Departments_DepartmentId",
+                        column: x => x.DepartmentId,
+                        principalTable: "Departments",
+                        principalColumn: "DepartmentId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DepartmentStudent_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "StudentId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DepartmentStudent_DepartmentId",
+                table: "DepartmentStudent",
+                column: "DepartmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DepartmentStudent_StudentId",
+                table: "DepartmentStudent",
+                column: "StudentId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DepartmentStudent");
+
+            migrationBuilder.DropTable(
+                name: "Departments");
+        }
+    }
+}
